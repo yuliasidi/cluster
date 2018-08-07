d43c289b31d8d72fe9856db38bf6ba16a40c8457
Yulia Sidi <yulia.sidi@uconn.edu>
2018-08-07 06:55:00 -0400

    add packrat

--- calcpi.R
+++ calcpi.R
@@ -3,7 +3,8 @@
 
 # Prepare: collect command line arguments,
 # set iteration number and a unique seed
-args <- commandArgs()
+#args <- commandArgs()
+args <- c('10000000','condor/data/pihat-1.rds')
 set.seed(Sys.time())
 n <- as.numeric(args[length(args)-1])
 
@@ -13,5 +14,5 @@ y <- runif(n)
 
 # Compute and output the value of pi
 pihat <- sum(x * x + y * y < 1) / n * 4
-save(pihat,file = args[length(args)])
+saveRDS(pihat,file = args[length(args)])
 proc.time()
